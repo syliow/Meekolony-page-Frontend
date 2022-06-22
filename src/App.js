f16931cd53994113113497b7b79df1bc29bf5f21
Shanyi Liow <shanyiliow1107@gmail.com>
2022-06-22 22:03:12 +0800

    wip: Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { axiosInstance } from "./config";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [listings, setListings] = useState([]);
+  const [sales, setSales] = useState([]);
+
+  const fetchMarketplaceData = async () => {
+    const { data } = axiosInstance.get("/nft");
+    if (data) {
+      setData(data);
+      console.log(data, "dataa");
+    } else {
+      setData("No data!");
+    }
+  };
+
+  const fetchNftListings = async () => {
+    const { data } = axiosInstance.get("/nft/listings");
+    if (data) {
+      setListings(data);
+      console.log(data, "dataa");
+    } else {
+      setListings("No data!");
+    }
+  };
+  const fetchNftSales = async () => {
+    const { data } = axiosInstance.get("/nft/sales");
+    if (data) {
+      setSales(data);
+      console.log(data, "dataa");
+    } else {
+      setSales("No data!");
+    }
+  };
+
+  useEffect(() => {
+    fetchMarketplaceData();
+    fetchNftListings();
+    fetchNftSales();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Frontend</h1>
+      <h3>{data}</h3>
+      <h3>{listings}</h3>
+      <h3>{sales}</h3>
     </div>
   );
 }
