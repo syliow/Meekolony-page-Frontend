4aa07a89443b9490b5cd52284bd03803d3ac4a37
Shanyi Liow <shanyiliow1107@gmail.com>
2022-06-23 14:53:11 +0800

    chore: fetch more nft listings when button is clicked

--- src/components/cardGrid.js
+++ src/components/cardGrid.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-
 import Typography from "@material-ui/core/Typography";
 import {
   Button,
@@ -26,46 +25,62 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ImgMediaCard() {
   const [listings, setListings] = useState([]);
+  const [startIndex, setStartIndex] = React.useState(0);
   const classes = useStyles();
+
+  const handleFetchListings = async () => {
+    setStartIndex((prev) => prev + 20);
+  };
 
   useEffect(() => {
     const fetchNftListings = async () => {
-      const { data } = axiosInstance.get("/nft/listings").then((res) => {
-        setListings(res.data);
-      });
+      const { data } = axiosInstance
+        .get("/nft/listings", {
+          params: {
+            startIndex,
+          },
+        })
+        .then((res) => {
+          setListings((prev) => [...prev, ...res.data]);
+        });
     };
 
     fetchNftListings();
-  }, []);
+  }, [startIndex]);
 
   return (
-    <Grid container className={classes.root} spacing={2}>
-      {listings.map((listing) => (
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Contemplative Reptile"
-              height="140"
-              image={listing.extra.img}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Meekolony #
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Price: {listing.price}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Details
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
-    </Grid>
+    <>
+      <Grid container className={classes.root} spacing={2}>
+        {listings.map((listing) => (
+          <Card className={classes.root}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                alt="Contemplative Reptile"
+                height="140"
+                image={listing.extra.img}
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Meekolony #
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Price: {listing.price}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button size="small" color="primary">
+                Details
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
+      </Grid>
+      <Button onClick={handleFetchListings} style={{ backgroundColor: "red", marginTop:"30px" }}>
+        Load More
+      </Button>
+    </>
   );
 }
