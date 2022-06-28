f8c747cedb5c572bc75d49847f13b7efb979ab1a
Shanyi Liow <shanyiliow1107@gmail.com>
2022-06-28 09:12:57 +0800

    feat: check for meekolony nft in wallet

--- src/components/AddressChecker.js
+++ src/components/AddressChecker.js
@@ -2,6 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import { Typography } from "@material-ui/core";
 import { axiosInstance } from "../config";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,15 +19,33 @@ export default function AddressChecker() {
 
   const [address, setAddress] = React.useState("");
   const [isLoading, setLoading] = React.useState(false);
+  const [result, setResult] = React.useState();
+
+  const handleClearUserInput = () => {
+    setAddress("");
+    setResult();
+  }
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
-    const res = await axiosInstance.get("/nft/checkAddress", {
-      params: {
-        userAddress: address,
-      },
-    });
+    await axiosInstance
+      .get("/nft/checkAddress", {
+        params: {
+          userAddress: address,
+        },
+      })
+      .then((res) => {
+        if (res) {
+          setResult(res.data);
+        }
+        setLoading(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        console.log(err, " error");
+      });
   };
 
   return (
@@ -51,6 +70,26 @@ export default function AddressChecker() {
       >
         Submit
       </Button>
+      <Button
+        variant="outlined"
+        color="primary"
+        style={{ marginTop: "20px" }}
+        onClick={handleClearUserInput}
+      >
+        Clear
+      </Button>
+
+      {/* {isLoading ? (
+        <Typography variant="h6" style={{ marginTop: "20px" }}>
+          Loading...
+        </Typography>
+      ) : (
+        <Typography>Please enter your address.</Typography>
+      )} */}
+      <Typography>
+        Total Meekolony NFT: {result ? result.length : "No Result"}
+      </Typography>
+      {result && result.map((nft) => <Typography>- {nft.name}</Typography>)}
     </form>
   );
 }
